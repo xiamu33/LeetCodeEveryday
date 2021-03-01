0fbf1ba2cad7d7866e049bd2647aabf61bac004a
xiamu <myjyixi@163.com>
2021-03-01 23:55:00 +0800

    feat: 2021-3-1
    [303] 区域和检索 - 数组不可变

---
+++ 303.区域和检索-数组不可变.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  this.nums = nums;
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (i, j) {
+  // (224 ms 43.9 MB)
+  let sum = 0;
+  for (let k = i; k <= j; k++) {
+    sum += this.nums[k];
+  }
+  return sum;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
+// @lc code=end
+
+const numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+console.log(numArray.sumRange(0, 2)); // 1
+console.log(numArray.sumRange(2, 5)); // -1
+console.log(numArray.sumRange(0, 5)); // -3
